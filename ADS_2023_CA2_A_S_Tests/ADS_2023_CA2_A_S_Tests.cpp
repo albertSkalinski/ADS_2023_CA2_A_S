e6f93d9980121976b3c7ac360e7aba0c80fafda2
albertSkalinski <D00248346@student.dkit.ie>
2023-12-13 18:00:46 +0000

    :white_check_mark: Added tests for Tree from Moodle

--- ADS_2023_CA2_A_S_Tests/ADS_2023_CA2_A_S_Tests.cpp
+++ ADS_2023_CA2_A_S_Tests/ADS_2023_CA2_A_S_Tests.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../ADS_2023_CA2_A_S_App/Tree.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +10,30 @@ namespace ADS2023CA2ASTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestConstructor)
 		{
+			Tree<int> tree(1);
+			Assert::AreEqual(1, tree.data);
+			Assert::IsNull(tree.parent);
+			Assert::IsNotNull(tree.children);
+			Assert::AreEqual(0, tree.children->size());
+		}
+		TEST_METHOD(TestCountOneItem)
+		{
+			Tree<int> tree(1);
+			Assert::AreEqual(1, tree.count());
+		}
+		TEST_METHOD(TestCountOneItemWithChildrenAndSubChildren)
+		{
+			Tree<int> tree(1);
+			Tree<int> childOne(2);
+			Tree<int> childTwo(3);
+			Tree<int> subChild(4);
+			childTwo.children->append(&subChild);
+			tree.children->append(&childOne);
+			tree.children->append(&childTwo);
+			Assert::AreEqual(2, childTwo.count());
+			Assert::AreEqual(4, tree.count());
 		}
 	};
 }
