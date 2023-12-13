ed4004c72267d5cb5d8e5063c73a17c43b76796f
albertSkalinski <D00248346@student.dkit.ie>
2023-12-13 17:46:21 +0000

    Add project files.

---
+++ ADS_2023_CA2_A_S_Tests/ADS_2023_CA2_A_S_Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ADS2023CA2ASTests
+{
+	TEST_CLASS(ADS2023CA2ASTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
