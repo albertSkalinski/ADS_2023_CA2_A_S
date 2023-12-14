db9d1e2b2a528520ae0a9b9f96843bf7b781a9f5
albertSkalinski <D00248346@student.dkit.ie>
2023-12-14 02:44:28 +0000

    :art: XMLTReeCreator retouched

--- ADS_2023_CA2_A_S_App/XMLTreeCreator.cpp
+++ ADS_2023_CA2_A_S_App/XMLTreeCreator.cpp
@@ -10,18 +10,20 @@
 
 using namespace std;
 
-// Function to trim leading and trailing whitespaces from a string
-string trim(const string& str) {
+//Trimming any whitespaces
+string trim(const string& str) 
+{
     size_t first = str.find_first_not_of(' ');
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last - first + 1));
 }
 
-// Function to load XML data into a tree
-Tree<string>* loadXmlData(const string& filename) {
+//Loading the XML data
+Tree<string>* loadXmlData(const string& filename) 
+{
     ifstream file(filename);
-    if (!file.is_open()) {
-        
+    if (!file.is_open()) 
+    {
         cerr << "Error: Unable to open file " << filename << endl;
         return nullptr;
     }
@@ -31,52 +33,57 @@ Tree<string>* loadXmlData(const string& filename) {
     Tree<string>* currentNode = nullptr;
 
     string line;
-    while (getline(file, line)) {
+    while (getline(file, line)) 
+    {
         line = trim(line);
 
-        if (line.empty()) {
-            continue; // Skip empty lines
+        //Skipping empty lines
+        if (line.empty()) 
+        {
+            continue;
         }
 
-        if (line.find("<?xml") != string::npos || line.find("<!DOCTYPE") != string::npos) {
-            continue; // Skip XML declaration and DOCTYPE
+        //Skipping the XML header
+        if (line.find("<?xml") != string::npos || line.find("<!DOCTYPE") != string::npos) 
+        {
+            continue;
         }
 
-        if (line.find("<filesystem>") != string::npos) {
-            // Initialize the root of the tree
+        //Initialising the root
+        if (line.find("<filesystem>") != string::npos) 
+        {
             root = new Tree<string>("filesystem");
             treeStack.push(root);
         }
-        else if (line.find("</filesystem>") != string::npos) {
-            // Pop the root from the stack
+        else if (line.find("</filesystem>") != string::npos) 
+        {
             treeStack.pop();
         }
-        else if (line.find("<directory") != string::npos) {
-            // Extract directory name
+        else if (line.find("<directory") != string::npos) 
+        {
+            //Taking the name of the directory
             string name = line.substr(line.find("name=\"") + 6);
             name = name.substr(0, name.find("\""));
 
-            // Create a new directory node
+            //Creating its node
             Tree<string>* directory = new Tree<string>("directory");
             directory->data = name;
 
-            // Add the directory node to the current node
             currentNode = treeStack.top();
             currentNode->children->append(directory);
 
-            // Push the directory node onto the stack
             treeStack.push(directory);
         }
-        else if (line.find("<file") != string::npos) {
-            // Extract file name
+        else if (line.find("<file") != string::npos) 
+        {
+            //Getting file's name
             string name = line.substr(line.find("name=\"") + 6);
             name = name.substr(0, name.find("\""));
 
-            // Create a new file node
+            // Creating its ndoe
             Tree<string>* fileNode = new Tree<string>("file");
             fileNode->data = name;
 
-            // Add the file node to the current node
             currentNode = treeStack.top();
             currentNode->children->append(fileNode);
         }
@@ -86,22 +93,24 @@ Tree<string>* loadXmlData(const string& filename) {
     return root;
 }
 
-
-
-// Function to print the tree structure
-void printTree(Tree<string>* node, int depth) {
-    if (node == nullptr) {
+//Printing the tree
+void printTree(Tree<string>* node, int depth) 
+{
+    if (node == nullptr) 
+    {
         return;
     }
 
-    for (int i = 0; i < depth; ++i) {
+    for (int i = 0; i < depth; ++i) 
+    {
         cout << "  ";
     }
 
     cout << node->getData() << endl;
 
     DListIterator<Tree<string>*> iter = node->children->getIterator();
-    while (iter.isValid()) {
+    while (iter.isValid()) 
+    {
         printTree(iter.item(), depth + 1);
         iter.advance();
     }
